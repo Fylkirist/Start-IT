aaaa54097d149ff06dfece2c033f4ce1faa12ddd
Fylkirist <112450556+Fylkirist@users.noreply.github.com>
2023-02-13 04:39:30 +0100

    Ryddigere

--- Minipoll/model.js
+++ Minipoll/model.js
@@ -1,7 +1,14 @@
 const model={
     currentPage:0,
     pollQuestions:[
-        {question:"How good is this page?",answers:[{answer:"Great",votes:0},{answer:"Fair",votes:0},{answer:"Terrible",votes:0}]}
+        {
+            question:"How good is this page?",
+            answers:[
+                {answer:"Great",votes:0},
+                {answer:"Fair",votes:0},
+                {answer:"Terrible",votes:0}
+            ]
+        }
     ],
     settingsPageQuestionInput:"",
     settingsPageAnswerInputList:[""],
